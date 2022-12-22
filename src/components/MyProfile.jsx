a9758fd439bc2e16d3886ef4da217be0614491a8
Ben Sales <adebsa2401@gmail.com>
2022-12-22 13:08:54 +0100

    feat: add reserved missions to Profile page

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MissionProfile from './MissionProfile';
 import RocketProfile from './RocketProfile';
 
 const MyProfile = () => (
 
   <main className="profile-main">
+    <MissionProfile />
     <RocketProfile />
   </main>
 );
